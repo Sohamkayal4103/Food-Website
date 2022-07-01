6f57b55319dc3fccf4562d96f8d0eb22d95d713c
Soham <kayalsoham61@gmail.com>
2022-07-02 00:28:50 +0530

    Fixed some Bugs

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -18,7 +18,7 @@ const Cards = ({key,id,name,image,category,area,instructions,youtube}) => {
         <div className="p-4 py-6">
           <h2>{name}</h2>
           <p className="text-sm text-gray-400">
-            {category}|{area}
+            {category} | {area}
           </p>
         </div>
         <div>
@@ -33,19 +33,19 @@ const Cards = ({key,id,name,image,category,area,instructions,youtube}) => {
       {showModal ?
         (
           <div className=" fixed inset-0 z-50 overflow-x-hidden overflow-y-auto  items-center">
-          <div className="md:w-2/3 mx-4 mt-24 md:mx-64 w-4/5 bg-white p-2 w-screen ml-10 rounded-lg">
+          <div className="md:w-2/3 mt-24 md:mx-64 w-4/5 bg-white p-2 w-screen rounded-lg">
             <div className="flex justify-center mt-4">
               <img src={image} alt="" className="rounded-full object-cover w-3/12 "/>
             </div>
             <div className="py-6 m-2">
-              <h2>Recipe Name</h2>
+              <h2 className="text-center">{name}</h2>
               <h2>Instructions:</h2>
               <p className="text-center text-sm px-2 mt-2">
               {instructions}
               </p>
               <div class="flex justify-end mt-4">
-                <a href ="/" className="shadow-lg p-2 rounded-md text-md m-2" rel="noopener noreferrer" target="_blank">Watch video</a>
-                <button onClick={handleClick} className="shadow-lg p-2 rounded-md text-md m-2">Close</button>
+                <a href ={youtube} className="shadow-lg p-2 rounded-md text-md m-2 hover:bg-neutral-300" rel="noopener noreferrer" target="_blank">Watch video</a>
+                <button onClick={handleClick} className="hover:bg-neutral-300 shadow-lg p-2 rounded-md text-md m-2">Close</button>
               </div>
             </div>
           </div>
