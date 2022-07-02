ce60d176cec2d3e9dd32b1b7ef9f4351d9e5c7f3
Soham <kayalsoham61@gmail.com>
2022-07-02 22:41:16 +0530

    Links and shortcut icon updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
       
       <h1 className = " text-lg">Connect with us:</h1>
       <div className="grid md:grid-cols-5 grid-cols-1 mt-6 gap-4">
-        <div className="hover:text-yellow-400 cursor-pointer"><a href="/"><ion-icon name="logo-linkedin"></ion-icon> LinkedIn</a></div>
-        <div className="hover:text-yellow-400 cursor-pointer"><a href="/"><ion-icon name="logo-github"></ion-icon> Github</a></div>
-        <div className="hover:text-yellow-400 cursor-pointer"><a href="/"><ion-icon name="logo-twitter"></ion-icon> Twitter</a></div>
-        <div className="hover:text-yellow-400 cursor-pointer"><a href="/"><ion-icon name="logo-instagram"></ion-icon> Instagram</a></div>
-        <div className="hover:text-yellow-400 cursor-pointer"><a href="/"><ion-icon name="logo-youtube"></ion-icon> YouTube</a></div>
+        <div className="hover:text-yellow-400 cursor-pointer"><a href="https://www.linkedin.com/in/soham-kayal-90939720b/"><ion-icon name="logo-linkedin"></ion-icon> LinkedIn</a></div>
+        <div className="hover:text-yellow-400 cursor-pointer"><a href="https://github.com/Sohamkayal4103"><ion-icon name="logo-github"></ion-icon> Github</a></div>
+        <div className="hover:text-yellow-400 cursor-pointer"><a href="https://twitter.com/kayal_soham"><ion-icon name="logo-twitter"></ion-icon> Twitter</a></div>
+        <div className="hover:text-yellow-400 cursor-pointer"><a href="https://www.instagram.com/sohamkayal/"><ion-icon name="logo-instagram"></ion-icon> Instagram</a></div>
+        <div className="hover:text-yellow-400 cursor-pointer"><a href="https://www.youtube.com/channel/UCIdHJ31IXugn2ec7yio3yQA"><ion-icon name="logo-youtube"></ion-icon> YouTube</a></div>
       </div>
       <h2 className = "mt-4">Developed using <span className="text-yellow-400">React</span> and <span className="text-yellow-400">Tailwind CSS</span></h2>
     </div>
